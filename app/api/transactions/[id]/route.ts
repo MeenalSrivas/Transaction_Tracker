4e55a9d6cf5aeeeb48f21f0979ef31d9675b89a7
meenal <meenalsrivastav2004@gamil.com>
2025-07-06 22:51:36 +0530

    corrected TypeScript typing issue specific to Vercel’s/Next.js’s route handler signatures

--- app/api/transactions/[id]/route.ts
+++ app/api/transactions/[id]/route.ts
@@ -1,9 +1,11 @@
-import { NextResponse } from 'next/server';
+import { NextRequest } from "next/server";
 import { connectDB } from '../../../../lib/mongodb';
 import {Transaction} from '../../../../lib/models/transaction';
 
-export async function DELETE(_: Request, { params }: { params: { id:string } }) {
+export async function DELETE(request: NextRequest, context: { params: { id: string } }) {
   await connectDB();
-  await Transaction.findByIdAndDelete(params.id);
-  return NextResponse.json({ success: true });
+  const { id } = context.params;
+  await Transaction.findByIdAndDelete(id);
+  return new Response(JSON.stringify({ success: true }), { status: 200 });
+ 
 }
